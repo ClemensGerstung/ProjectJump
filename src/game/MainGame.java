e403ddedf5efb8275fb847aaecc7666262543962
Alexander <i@d.m>
2015-03-13 20:01:50 +0100

    Test

--- src/game/MainGame.java
+++ src/game/MainGame.java
@@ -10,7 +10,7 @@ public class MainGame {
 		AppGameContainer jumper = new AppGameContainer(new GameApp("Jump And Run alpha 0.8.5"));
         jumper.setDisplayMode(800, 450, false);
         jumper.setAlwaysRender(true);
-        jumper.setShowFPS(false); // TODO: set to false
+        jumper.setShowFPS(true); // TODO: set to false
         jumper.start();
 	}
 
